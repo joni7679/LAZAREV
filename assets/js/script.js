e7d2242ce5db13dc8816bd9f016c283023d219dd
joni <codingmylife0@gmail.com>
2024-04-17 00:29:38 +0530

    update and responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector(".main"),
-    smooth: true,
-})
+function locomotiveScrollAnimation() {
+
+    gsap.registerPlugin(ScrollTrigger);
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+}
+
+locomotiveScrollAnimation()
+
 function navBarAnimation() {
     let navBar = document.querySelector("nav");
     navBar.addEventListener("mouseenter", () => {
@@ -49,7 +77,7 @@ function navBarAnimation() {
 
     })
 }
-navBarAnimation()
+// navBarAnimation()
 
 function imgcursorEffect() {
     let rightElem = document.querySelectorAll(".right-elem");
@@ -128,11 +156,51 @@ function customcursorEffect() {
 
         })
 
-
     })
-
-
 
 }
 
 customcursorEffect()
+
+
+function productSectionAnimation() {
+    gsap.from("#product-btn-part1 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#product-btn-part1",
+            scroller: ".main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true,
+        }
+    });
+}
+
+productSectionAnimation()
+
+function loadingAnimation() {
+    let tl = gsap.timeline()
+    tl.from(".hero-section", {
+        opacity: 0,
+        duration: 0.1,
+        dealy: 0.2,
+    })
+
+    tl.from(".hero-section", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "150px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0
+    })
+
+    tl.from(".hero-section h1, .hero-section p, .hero-section div", {
+        opacity: 0,
+        stagger: 0.2
+    })
+}
+loadingAnimation()
